420e3168b1142b55448fcde4ed49ec6a9d101fed
Sportik99 <shchetinin.ase@yandex.ru>
2025-04-05 13:00:26 +0600

    Initial commit

---
+++ src/main/java/sskier/pet/scheduling_meetings_common/SchedulingMeetingsCommonApplication.java
@@ -0,0 +1,13 @@
+package sskier.pet.scheduling_meetings_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingMeetingsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingMeetingsCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sskier/pet/scheduling_meetings_common/SchedulingMeetingsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package sskier.pet.scheduling_meetings_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingMeetingsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
